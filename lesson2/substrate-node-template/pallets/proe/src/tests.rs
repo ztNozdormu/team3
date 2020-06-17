d1ba8f2a8d27b463c4cab69c6fe78daedda5ae61
ztNozdormu <w1999wtw3537@sina.com>
2020-06-17 20:10:41 +0800

    第三课作业补交

--- lesson2/substrate-node-template/pallets/proe/src/tests.rs
+++ lesson2/substrate-node-template/pallets/proe/src/tests.rs
@@ -1,6 +1,19 @@
 // Tests to be written here
 
 use crate::{Error, mock::*};
+use super::*;
 use frame_support::{assert_ok, assert_noop};
 
-
+// test cases for create_claim
+#[test]
+fn create_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        assert_ok!(PoeModule::create_claim(Origin::Signed(1),claim.clone()));
+        assert_eq!(Proofs::<Test>::get(&claim),(1,system::Module::<Test>::block_number()));
+    })
+}
+#[test]
+fn create_claim_failed_when_claim_already_exist(){
+    new_test_ext().execute
+}
